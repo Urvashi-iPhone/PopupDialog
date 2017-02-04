471d620cc5f202206088b054bf890224ce872223
Urvashi-iPhone <lathiyaurvashi36@gmail.com>
2017-02-04 10:16:22 +0530

    Initial Commit

---
+++ PopUp Menu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PopUp Menu
+//
+//  Created by Tecksky Techonologies on 2/4/17.
+//  Copyright © 2017 Tecksky Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
