aa2f4684ab6300265405f324c2c3ba022936a6d6
Urvashi-iPhone <lathiyaurvashi36@gmail.com>
2017-02-04 14:08:41 +0530

    first

--- PopUp Menu/ViewController.m
+++ PopUp Menu/ViewController.m
@@ -7,23 +7,110 @@
 //
 
 #import "ViewController.h"
+#import "KOPopupView.h"
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UIView *alertView;
+@property (weak, nonatomic) IBOutlet UIButton *camerabtn;
+@property (weak, nonatomic) IBOutlet UIImageView *logoimg;
+@property (weak, nonatomic) IBOutlet UIButton *gallarybtn;
+@property (weak, nonatomic) IBOutlet UIImageView *imageuplod;
+@property (weak, nonatomic) IBOutlet UIButton *canclebtn;
+@property (nonatomic, strong) KOPopupView *popup;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    _alertView.hidden = YES;
+   
+    _camerabtn.layer.cornerRadius = _camerabtn.frame.size.height /2;
+    _gallarybtn.layer.cornerRadius = _gallarybtn.frame.size.height /2;
+    _canclebtn.layer.cornerRadius = _canclebtn.frame.size.height /2;
+    _logoimg.layer.cornerRadius = _logoimg.frame.size.height /2;
+    _logoimg.layer.masksToBounds = YES;
+    _logoimg.layer.borderWidth = 0;
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)showPressed:(id)sender{
+      _alertView.hidden = NO;
+    if(!self.popup)
+        self.popup = [KOPopupView popupView];
+    [self.popup.handleView addSubview:self.alertView];
+    self.alertView.center = CGPointMake(self.popup.handleView.frame.size.width/2.0,self.popup.handleView.frame.size.height/2.0);
+    [self.popup show];
+    
 }
+
+- (IBAction)hidePressed:(id)sender{
+    [self.popup hideAnimated:YES];
+}
+
+- (void)willRotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration{
+    [self.popup willRotateToOrientation:toInterfaceOrientation withDuration:duration];
+}
+- (IBAction)camerabtn:(id)sender {
+    
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera])
+    {
+        UIImagePickerController *picker = [[UIImagePickerController alloc]init];
+        picker.delegate = self;
+        picker.sourceType = UIImagePickerControllerSourceTypeCamera;
+        picker.allowsEditing = YES;
+        
+        [self presentModalViewController:picker animated:YES];
+        
+      [self.popup hideAnimated:YES];
+        
+    }
+    else
+    {
+        UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"Camera Unavailable"
+                                                       message:@"Unable to find a camera on your device."
+                                                      delegate:nil
+                                             cancelButtonTitle:@"OK"
+                                             otherButtonTitles:nil, nil];
+        [alert show];
+        alert = nil;
+        
+    }
+}
+- (IBAction)gallarybtn:(id)sender {
+    
+    NSLog(@"gallary");
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    picker.delegate = self;
+    picker.allowsEditing = YES;
+    picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    
+    [self presentViewController:picker animated:YES completion:NULL];
+    [self.popup hideAnimated:YES];
+    
+}
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    [self dismissViewControllerAnimated:YES completion:nil];
+  //  UIImage *image = info[UIImagePickerControllerOriginalImage];
+    UIImage *chosenImage = info[UIImagePickerControllerEditedImage];
+    if (chosenImage) {
+        
+       
+        self.imageuplod.image = chosenImage;
+        
+        [picker dismissViewControllerAnimated:YES completion:NULL];
+     
+      
+    }
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
+{
+    
+    [picker dismissViewControllerAnimated:YES completion:NULL];
+    
+}
+
 
 
 @end
